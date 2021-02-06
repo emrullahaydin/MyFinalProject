66d3d42b352ed6a73fa476b268ac389d20ae03ff
Emrullah AYDIN <61662213+emrullahaydin@users.noreply.github.com>
2021-02-06 20:35:18 +0300

    03.02.2021 Dersi Bitişi

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : Db tabloları ile proje classlarının bağlamak
+   public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+          optionsBuilder.UseSqlServer(@"Data Source=BT\SQLSERVER;Initial Catalog=Northwind;User ID=sa;Password=Emo_93351986");            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -14,11 +15,11 @@ namespace DataAccess.Concrete.InMemory
         {
             //Dışardan veri gekliyormuş gibi düşünelim. Dışardan belleğe veri yüklüyoruz... Biz manuel yazdık.
             _products = new List<Product> {
-                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitInStock=15},
-                new Product{ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitInStock=3},
-                new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitInStock=2},
-                new Product{ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitInStock=65},
-                new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=85,UnitInStock=1},
+                new Product{ProductId=2,CategoryID=1,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
+                new Product{ProductId=3,CategoryID=2,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2},
+                new Product{ProductId=1,CategoryID=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                new Product{ProductId=4,CategoryID=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
+                new Product{ProductId=5,CategoryID=2,ProductName="Fare",UnitPrice=85,UnitsInStock=1},
             };
         }
         public void Add(Product product)
@@ -44,14 +45,24 @@ namespace DataAccess.Concrete.InMemory
             _products.Remove(productToDelete);
         }
 
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Product> GetAll()
+        {
+            return _products;            
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
         {
             return _products;
         }
 
         public List<Product> GetByCategory(int categoryId)
         {
-            return _products.Where(p => p.CategoryId == categoryId).ToList();
+            return _products.Where(p => p.CategoryID == categoryId).ToList();
         }
 
         public void Update(Product product)
@@ -59,9 +70,9 @@ namespace DataAccess.Concrete.InMemory
             //Gönderdiğim ürün id sine sahip olan listedeki ürünü bul demek.
             Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
             productToUpdate.ProductName = product.ProductName;
-            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.CategoryID = product.CategoryID;
             productToUpdate.UnitPrice = product.UnitPrice;
-            productToUpdate.UnitInStock = product.UnitInStock;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
         }
     }
 }
